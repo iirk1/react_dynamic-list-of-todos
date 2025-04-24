7074127fc2f9f050dead67bb56da3b5577716942
Iryna <irakogut23190@gmail.com>
2025-04-24 14:12:49 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,58 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
-import { Loader } from './components/Loader';
+import { getTodos, getUser } from './api';
+import { User } from './types/User';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState([]);
+  const [visibleTodos, setVisibleTodos] = useState([]);
+  const [user, setUser] = useState<User | null>();
+  const [modalWindow, setModalWindow] = useState(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [userLoading, setUserLoading] = useState<boolean>(false);
+
+  const [currentTodo, setCurrentTodo] = useState<Todo | null>();
+
+  useEffect(() => {
+    setLoading(true);
+    getTodos().then((res: Todo[]) => {
+      setTodos(res);
+      setVisibleTodos(res);
+      setLoading(false);
+    });
+  }, []);
+
+  const handleSetUser = (userId: number) => {
+    setUserLoading(true);
+    getUser(userId)
+      .then(res => {
+        setUser(res);
+      })
+      .finally(() => {
+        setModalWindow(true);
+        setUserLoading(false);
+      });
+  };
+
+  const handleModalWindow = (value: boolean) => {
+    setModalWindow(value);
+  };
+
+  const handleSetVisibleTodos = (value: Todo[]) => {
+    setVisibleTodos(value);
+  };
+
+  const handleSetLoading = (value: boolean) => {
+    setLoading(value);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +61,36 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                todos={todos}
+                setVisibleTodos={handleSetVisibleTodos}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              <TodoList
+                modalWindow={modalWindow}
+                todos={visibleTodos}
+                loading={loading}
+                getCurrentUser={handleSetUser}
+                currentTodo={currentTodo}
+                setModalWindow={handleModalWindow}
+                getCurrentTodo={(value: Todo[]) => setCurrentTodo(value)}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {modalWindow && (
+        <TodoModal
+          modalWindow={modalWindow}
+          loading={userLoading}
+          currentUser={user}
+          currentTodo={currentTodo}
+          setModalWindow={handleModalWindow}
+        />
+      )}
     </>
   );
 };
